4da712621d1884d4af63ee0412fa8733638be4e2
Bryan Ramos <bryan@ramos.codes>
2023-04-19 17:21:40 -0400

    Formatted

--- homeConfig/dotfiles/nvim/init.lua
+++ homeConfig/dotfiles/nvim/init.lua
@@ -6,15 +6,6 @@
 -- Enable clipboard
   vim.o.clipboard = "unnamedplus"
 
-
--- Load packer.nvim
-  local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if  vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
-        vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. packer_install_path)
-        vim.cmd 'packadd packer.nvim'
-  end
-  dofile(vim.fn.stdpath('config') .. '/plugins.lua')
-
 -- Keep Cursor
   vim.o.guicursor = ''
 
@@ -24,12 +15,21 @@
   vim.opt.expandtab = true
   vim.opt.tabstop = 2
 
+-- Init Snippets
+  vim.g.snipMate = { snippet_version = 1 }
+
+-- Load packer.nvim
+  local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if  vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
+        vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. packer_install_path)
+        vim.cmd 'packadd packer.nvim'
+  end
+  dofile(vim.fn.stdpath('config') .. '/plugins.lua')
+
+
 -- Treesitter Enable
   require'nvim-treesitter.configs'.setup {
     highlight = {
       enable = true,
     },
   }
-
--- Init Snippets
-  vim.g.snipMate = { snippet_version = 1 }
