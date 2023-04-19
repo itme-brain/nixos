cb0d9961c1c544fc42784459e678e1682496d5cc
Bryan Ramos <bryan@ramos.codes>
2023-04-19 16:27:26 -0400

    Fixed lsp?

--- homeConfig/dotfiles/nvim/plugins.lua
+++ homeConfig/dotfiles/nvim/plugins.lua
@@ -90,29 +90,29 @@ return packer.startup(function(use)
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-compe'
   use 'kabouzeid/nvim-lspinstall'
-
-  local nvim_lsp = require('lspconfig')
-  local lspinstall = require('lspinstall')
-
-  lspinstall.setup()
-
-  for _, lsp in ipairs(lspinstall.installed_servers()) do
-      nvim_lsp[lsp].setup {
-          on_attach = function(client, bufnr)
-              client.resolved_capabilities.document_formatting = false
-              require('lsp_signature').on_attach()
-          end,
-      }
-  end
-
-  require('compe').setup({
-      enabled = true,
-      source = {
-          path = true,
-          buffer = true,
-          nvim_lsp = true,
-          nvim_lua = true,
-          treesitter = true,
-      },
-  })
 end)
+
+local nvim_lsp = require('lspconfig')
+local lspinstall = require('lspinstall')
+
+lspinstall.setup()
+
+for _, lsp in ipairs(lspinstall.installed_servers()) do
+    nvim_lsp[lsp].setup {
+	on_attach = function(client, bufnr)
+	    client.resolved_capabilities.document_formatting = false
+	    require('lsp_signature').on_attach()
+	end,
+    }
+end
+
+require('compe').setup({
+    enabled = true,
+    source = {
+	path = true,
+	buffer = true,
+	nvim_lsp = true,
+	nvim_lua = true,
+	treesitter = true,
+    },
+})
