21a6f28e52a3c760eb9aee4367dddbcd5faff4c2
Bryan Ramos <bryan@ramos.codes>
2023-04-19 16:31:43 -0400

    Fixed lsp?

--- homeConfig/dotfiles/nvim/plugins.lua
+++ homeConfig/dotfiles/nvim/plugins.lua
@@ -88,31 +88,7 @@ return packer.startup(function(use)
 
   -- LSP Config
   use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
   use 'kabouzeid/nvim-lspinstall'
+  
+  use 'hrsh7th/nvim-compe'
 end)
-
-local nvim_lsp = require('lspconfig')
-local lspinstall = require('lspinstall')
-
-lspinstall.setup()
-
-for _, lsp in ipairs(lspinstall.installed_servers()) do
-    nvim_lsp[lsp].setup {
-	on_attach = function(client, bufnr)
-	    client.resolved_capabilities.document_formatting = false
-	    require('lsp_signature').on_attach()
-	end,
-    }
-end
-
-require('compe').setup({
-    enabled = true,
-    source = {
-	path = true,
-	buffer = true,
-	nvim_lsp = true,
-	nvim_lua = true,
-	treesitter = true,
-    },
-})
