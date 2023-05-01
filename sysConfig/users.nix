e8717a6466b9b217561923cba7747d504b5a9340
Bryan Ramos <bryan@ramos.codes>
2023-05-01 06:34:38 -0400

    Added docker group

--- sysConfig/users.nix
+++ sysConfig/users.nix
@@ -3,7 +3,7 @@
 {
   users.users.bryan = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "home-manager" "input" "video" "audio" "kvm" "libvirtd" ]; 
+    extraGroups = [ "wheel" "networkmanager" "home-manager" "input" "video" "audio" "kvm" "libvirtd" "docker" ]; 
   };
 
   security.sudo.wheelNeedsPassword = false;
