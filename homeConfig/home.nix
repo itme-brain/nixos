993273487613eb05e60c88b0532b264643e381a1
Bryan Ramos <bryan@ramos.codes>
2023-05-23 16:08:44 -0400

    Move to bashrc

--- homeConfig/home.nix
+++ homeConfig/home.nix
@@ -64,9 +64,6 @@
 # NEOVIM
   programs.neovim = {
     enable = true;
-    viAlias = true;
-    vimAlias = true;
-
     plugins = with pkgs; [
       vimPlugins.nvim-treesitter.withAllGrammars
     ];
