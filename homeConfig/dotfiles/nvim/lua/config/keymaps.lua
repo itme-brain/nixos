247e548c817eacb9f4a56cc8f64be1c8320363b0
Bryan Ramos <bryan@ramos.codes>
2023-05-28 15:26:44 -0400

    Added new keymaps

--- homeConfig/dotfiles/nvim/lua/config/keymaps.lua
+++ homeConfig/dotfiles/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_set_keymap('n', '<PageUp>', '<PageUp>zz', {noremap = true})
+vim.api.nvim_set_keymap('n', '<PageDown>', '<PageDown>zz', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-U>', '<C-U>zz', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-D>', '<C-D>zz', {noremap = true})
