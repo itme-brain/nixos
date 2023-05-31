965a0f599474988edb4a9835db71a71648d63f8c
Bryan Ramos <bryan@ramos.codes>
2023-05-31 01:57:08 -0400

    test

--- homeConfig/dotfiles/nvim/lua/config/keymaps.lua
+++ homeConfig/dotfiles/nvim/lua/config/keymaps.lua
@@ -2,7 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_set_keymap('n', '<C-U>', '<C-U>zz', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-D>', '<C-D>zz', {noremap = true})
-vim.api.nvim_set_keymap('i', '<Tab>', '<Plug>luasnip-expand-or-jump', {})
-vim.api.nvim_set_keymap('s', '<Tab>', '<Plug>luasnip-expand-or-jump', {})
+vim.api.nvim_set_keymap('n', '<C-U>', '<C-U>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-D>', '<C-D>zz', { noremap = true })
