9226163811346fa2df5e9f191591a333f2323bc5
Bryan Ramos <bryan@ramos.codes>
2023-05-31 16:33:48 -0400

    Trying

--- homeConfig/dotfiles/nvim/lua/config/autocmds.lua
+++ homeConfig/dotfiles/nvim/lua/config/autocmds.lua
@@ -76,3 +76,9 @@ cmp.setup {
   },
 }
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- Disable virtual_text
+    virtual_text = false,
+  }
+)
