aba3c61906fb036dac18b96b151b9c887160a147
Bryan Ramos <bryan@ramos.codes>
2023-06-06 14:39:12 -0400

    Trying to fix nur

--- flake.nix
+++ flake.nix
@@ -2,19 +2,28 @@
 
   inputs = 
   { 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+      overlays = [
+        (self: super: {
+          nur = import (builtins.fetchTarball {
+            url = "https://github.com/nix-community/NUR/archive/3a6a6f4da737da41e27922ce2cfacf68a109ebce.tar.gz";
+            sha256 = "0g0vxzwm24pvfwhy8f320x030kvacxln6n3b50kwg14cjrirr2yx";
+          }) { inherit (super) pkgs; };
+        })
+      ];
+    };
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur.url = "github:nix-community/NUR";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, disko }:
+  outputs = { self, nixpkgs, home-manager, disko }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -29,7 +38,6 @@
       inherit system;
       modules = [
         ./sysConfig/desktop
-        nur.nixosModules.nur
         disko.nixosModules.disko
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
