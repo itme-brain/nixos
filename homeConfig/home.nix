c79e5fc3f41777d6cc48416c1dea8a609818d742
Bryan Ramos <bryan@ramos.codes>
2023-06-06 15:09:55 -0400

    cat

--- homeConfig/home.nix
+++ homeConfig/home.nix
@@ -2,7 +2,7 @@
 
 {
   programs.home-manager.enable = true;
-  imports = [ (import ./modules/default.nix { inherit pkgs config; }) ];
+  imports = [ (import ./modules/default.nix { inherit pkgs; }) ];
   home.stateVersion = "22.11";
 
   home.username = "bryan";
