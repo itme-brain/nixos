67540581775340096b089e0feb92b23697583187
Bryan Ramos <bryan@ramos.codes>
2023-06-07 21:16:19 -0400

    lets try again

--- flake.nix
+++ flake.nix
@@ -31,13 +31,12 @@
       modules = [
         ./sysConfig/desktop
         disko.nixosModules.disko
-      ];
-    };
-
-    homeConfigurations.bryan = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [
-        ./homeConfig/home.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.bryan = import ./homeConfig/home.nix;
+        }
       ];
     };
   };
