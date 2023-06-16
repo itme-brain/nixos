c7bcc6f2debdb28d7f10f7b6cbd4afb360f6dc09
Bryan Ramos <bryan@ramos.codes>
2023-06-16 12:44:32 -0400

    cat

--- homeConfig/modules/fun/default.nix
+++ homeConfig/modules/fun/default.nix
@@ -7,14 +7,16 @@ let
 in 
 { options.modules.fun = { enable = mkEnableOption "fun"; };
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      spotify
-      discord
-      steam
+    programs.obs-studio = {
+      enable = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-pipewire-audio-capture
+      ];
+    };
 
-      obs-studio
-      obs-studio-plugins.wlrobs
-      obs-studio-plugins.obs-pipewire-audio-capture
+    home.packages = with pkgs; [
+      spotify discord steam
     ];
   };
 }
