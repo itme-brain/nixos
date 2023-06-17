6e22df710bc55b0df400d0030037f9f04f141749
Bryan Ramos <bryan@ramos.codes>
2023-06-17 13:02:04 -0400

    defaults

--- homeConfig/modules/neovim/config/lazyvim/lua/config/keymaps.lua
+++ homeConfig/modules/neovim/config/lazyvim/lua/config/keymaps.lua
@@ -2,7 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-K>", "<C-U>zz", { silent = true })
-vim.keymap.set("n", "<C-J>", "<C-D>zz", { silent = true })
-vim.keymap.set("n", "<C-L>", "w", { silent = true })
-vim.keymap.set("n", "<C-H>", "b", { silent = true })
+vim.keymap.set("n", "<C-U>", "<C-U>zz", { silent = true })
+vim.keymap.set("n", "<C-D>", "<C-D>zz", { silent = true })
