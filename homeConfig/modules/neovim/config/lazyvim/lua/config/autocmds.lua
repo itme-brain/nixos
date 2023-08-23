d1f7ca93fd9aae112fedf8a005f23622438d8c71
Bryan Ramos <bryan@ramos.codes>
2023-08-23 17:10:03 -0400

    rm

--- homeConfig/modules/neovim/config/lazyvim/lua/config/autocmds.lua
+++ homeConfig/modules/neovim/config/lazyvim/lua/config/autocmds.lua
@@ -106,8 +106,8 @@ cmp.setup({
 	mapping = {
 		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<Down>"] = cmp.mapping.select_next_item(),
-		["<Up>"] = cmp.mapping.select_prev_item(),
+--	["<Down>"] = cmp.mapping.select_next_item(),
+--	["<Up>"] = cmp.mapping.select_prev_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 
