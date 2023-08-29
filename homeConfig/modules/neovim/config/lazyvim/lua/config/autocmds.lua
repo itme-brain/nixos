abb95f51b40c3c1bfffbff03af5961a81473939f
Bryan Ramos <bryan@ramos.codes>
2023-08-29 06:35:19 -0400

    on_attach

--- homeConfig/modules/neovim/config/lazyvim/lua/config/autocmds.lua
+++ homeConfig/modules/neovim/config/lazyvim/lua/config/autocmds.lua
@@ -22,26 +22,28 @@ require("notify").setup({
 local lsp = require("lsp-zero").preset({})
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({
+          bufnr = bufnr,
+          filter = function(client)
+            return client.name == "null-ls"
+          end,
+        })
+        vim.lsp.buf.formatting_sync()
+      end,
+    })
+  end
+end
+
 require("null-ls").setup({
 	-- you can reuse a shared lspconfig on_attach callback here
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						bufnr = bufnr,
-						filter = function(client)
-							return client.name == "null-ls"
-						end,
-					})
-					vim.lsp.buf.formatting_sync()
-				end,
-			})
-		end
-	end,
+  on_attach = on_attach,
 })
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
