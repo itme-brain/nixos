c98d81fa976b741e2aab4a7dbac5e5beee9aca28
Bryan Ramos <bryan@ramos.codes>
2024-01-15 13:37:05 -0500

    added git commit flag

--- homeConfig/modules/git/config/git.nix
+++ homeConfig/modules/git/config/git.nix
@@ -4,8 +4,9 @@
   userEmail = "bryan@ramos.codes";
   signing.key = "F1F3466458452B2DF351F1E864D12BA95ACE1F2D";
 
-  extraConfig = {
+  config = {
     init = { defaultBranch = "main"; };
+    commit = { gpgsign = true; };
   };
 
   ignores = [
