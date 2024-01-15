92ae9ae9bead6a835fbd97b1d4918a63e70e5ebe
Bryan Ramos <bryan@ramos.codes>
2024-01-15 14:09:33 -0500

    added bash server config

--- homeConfig/modules/bash/default.nix
+++ homeConfig/modules/bash/default.nix
@@ -1,22 +1,39 @@
 { lib, config, ... }:
 
 with lib;
-let 
+let
+  hostname = config.systemName;
   cfg = config.modules.bash;
-
-in 
+  socratesConfigs = {
+      initExtra = import ./config/desktop/prompt.nix;
+      profileExtra = import ./config/desktop/bashprofile.nix;
+      bashrcExtra = import ./config/desktop/bashrc.nix;
+      shellAliases = import ./config/desktop/alias.nix;
+  };
+  archimedesConfigs = {
+      initExtra = import ./config/server/prompt.nix;
+      profileExtra = import ./config/server/bashprofile.nix;
+      bashrcExtra = import ./config/server/bashrc.nix;
+      shellAliases = import ./config/server/alias.nix;
+  };
+  selectedConfig =
+    if hostname == "archimedes"
+      then archimedesConfigs
+    else socratesConfigs;
+in
 { options.modules.bash = { enable = mkEnableOption "bash"; };
   config = mkIf cfg.enable {
-    programs.bash = {
-      enable = true;
-      enableCompletion = true;
-
-      initExtra = import ./config/prompt.nix;
-      profileExtra = import ./config/bashprofile.nix;
-      bashrcExtra = import ./config/bashrc.nix;
-      shellAliases = import ./config/alias.nix;
+    programs = {
+      bash = {
+        enable = true;
+        enableCompletion = true;
+        initExtra = selectedConfig.initExtra;
+        profileExtra = selectedConfig.profileExtra;
+        bashrcExtra = selectedConfig.bashrcExtra;
+        shellAliases = selectedConfig.shellAliases;
+      };
     };
-    
+
     services.gpg-agent.enableBashIntegration = true;
     programs = {
       direnv = {
