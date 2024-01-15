9ede5b945279c0f6506b0514985e2d79b297a292
Bryan Ramos <bryan@ramos.codes>
2024-01-15 15:18:13 -0500

    added tmux module

---
+++ homeConfig/modules/tmux/default.nix
@@ -0,0 +1,15 @@
+{ lib, pkgs, config, ... }:
+
+with lib;
+  let cfg = config.modules.tmux;
+in
+{ options.modules.tmux = { enable = mkEnableOption "tmux"; };
+  config = mkIf cfg.enable {
+    programs = {
+      tmux = {
+        enable = true;
+        keyMode = "vi";
+      };
+    };
+  };
+}
