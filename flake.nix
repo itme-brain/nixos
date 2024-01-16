33a1fddde46cc7821045fa3a976232c38fc0d1e1
Bryan Ramos <bryan@ramos.codes>
2024-01-15 22:59:48 -0500

    added archimedes flake output

--- flake.nix
+++ flake.nix
@@ -34,5 +34,17 @@
         }
       ];
     };
+    nixosConfigurations.archimedes = nixpkgs.lib.nixosSystem {
+      inherit system pkgs;
+      modules = [
+        ./sysConfig/server
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.bryan = import ./homeConfig/home.nix;
+        }
+      ];
+    };
   };
 }
