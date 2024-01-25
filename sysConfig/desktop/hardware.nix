63dfa2a8182d349d4dc9dd46ec7687ab7328a598
Bryan Ramos <bryan@ramos.codes>
2024-01-25 18:59:18 -0500

    nit

--- sysConfig/desktop/hardware.nix
+++ sysConfig/desktop/hardware.nix
@@ -28,20 +28,20 @@
   };
 
 # FStab
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/af24c5b3-8a6e-4333-a61d-922a97928cae";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/af24c5b3-8a6e-4333-a61d-922a97928cae";
+    fsType = "ext4";
+  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/1639ee20-28d6-4649-814d-ba981c138b35";
-      fsType = "ext4";
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/1639ee20-28d6-4649-814d-ba981c138b35";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/74B9-4AAF";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/74B9-4AAF";
+    fsType = "vfat";
+  };
 
 # GPU
   hardware.nvidia.open = true;
