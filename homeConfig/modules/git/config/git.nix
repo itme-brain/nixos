2a67b95d79346afeb80414f505861e6975e28409
Bryan Ramos <bryan@ramos.codes>
2024-01-24 23:46:49 -0500

    signByDefault = true

--- homeConfig/modules/git/config/git.nix
+++ homeConfig/modules/git/config/git.nix
@@ -2,7 +2,10 @@
   enable = true;
   userName = "Bryan Ramos";
   userEmail = "bryan@ramos.codes";
-  signing.key = "F1F3466458452B2DF351F1E864D12BA95ACE1F2D";
+  signing = {
+    key = "F1F3466458452B2DF351F1E864D12BA95ACE1F2D";
+    signByDefault = true;
+  };
 
   extraConfig = {
     init = { defaultBranch = "main"; };
