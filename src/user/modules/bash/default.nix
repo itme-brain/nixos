89a5242da4d1742eef20711aabd345677059e23a
Bryan Ramos <bryan@ramos.codes>
2024-05-11 15:16:19 -0400

    set vi

--- src/user/modules/bash/default.nix
+++ src/user/modules/bash/default.nix
@@ -14,6 +14,10 @@ in
       initExtra = import ./config/prompt.nix { inherit lib config; };
       bashrcExtra = import ./config/bashrc.nix;
       shellAliases = import ./config/alias.nix;
+
+      shellOptions = [
+        "set -o vi"
+      ];
     };
 
     programs = {
