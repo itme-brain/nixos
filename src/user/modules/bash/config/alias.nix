d3ba585a490d9f9e544516f69fbe94acffe93898
Bryan Ramos <bryan@ramos.codes>
2024-05-14 22:57:26 -0400

    fixed aliases not building properly

--- src/user/modules/bash/config/alias.nix
+++ src/user/modules/bash/config/alias.nix
@@ -1,10 +1,9 @@
 { lib }:
 
 with lib;
-mkForce
 {
   cd = "cd -L";
   grep = "grep --color";
   tree = "eza --tree --icons=never";
-  lt = "eza --tree --icons=never";
+  lt = mkForce "eza --tree --icons=never";
 }
