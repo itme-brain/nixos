35cf3c5d4a4b638dbdf4e8d79a82c4cabbaf5cb8
Bryan Ramos <bryan@ramos.codes>
2024-05-15 20:35:52 -0400

    mkModules impl

--- src/user/modules/default.nix
+++ src/user/modules/default.nix
@@ -1,9 +1,14 @@
+{ lib, ... }:
+
+with lib;
+let
+  mkModules = dir: recursiveUpdate
+    (attrsets.mapAttrs (_: moduleDir: {
+      inherit (moduleDir) default;
+    }) (filterAttrs (n: v: isAttrs v) (attrsets.mapAttrs (_: v: builtins.readDir v) dir)))
+    { inherit (dir) default; };
+
+in
 {
-  imports = [
-    ./bash
-    ./git
-    ./gui
-    ./security
-    ./utils
-  ];
+  imports = mkModules ./.;
 }
