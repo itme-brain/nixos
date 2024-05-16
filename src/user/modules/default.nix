fab02b25dd6f78ce323d4bcec8b18295b17649ab
Bryan Ramos <bryan.ramos@concurrent-rt.com>
2024-05-16 09:59:04 -0400

    updated wallpapers implementation

--- src/user/modules/default.nix
+++ src/user/modules/default.nix
@@ -2,38 +2,17 @@
 
 with lib;
 let
-listDir = dir: builtins.filter (path: builtins.pathExists (dir + "/" + path)) (builtins.readDir dir);
 
-traverse = dir: pathList: {
-  inherit dir;
-  contents = builtins.foldl' (acc: path:
-  let
-    fullPath = dir + "/" + path;
-    newPathList = pathList ++ [ path ];
-    optionPath = builtins.concatStringsSep "." newPathList;
-    cfgPath = builtins.concatStringsSep "." (["config" "modules" "user"] ++ newPathList);
-  in
-    if builtins.pathExists fullPath && builtins.isDirectory fullPath then
-      if path == "modules" then
-        acc // { "${path}" = traverse fullPath newPathList; }
-      else
-        acc
-    else if path == "default.nix" then
-      let
-        userDefinedAttrs = builtins.evalFile fullPath;
-      in
-        acc // { "${builtins.head pathList}" = {
-          enable = mkEnableOption "user.${optionPath}";
-          config = mkIf (eval "${cfgPath}.enable") userDefinedAttrs;
-        }; }
-    else
-      acc
-  ) {} (listDir dir);
-};
-
-mkModules = dir: {
-  options.modules.user = traverse dir [];
-};
+mkModules = dir: (
+  listToAttrs (
+    map (module: {
+      name = module;
+      value = if module == "default.nix" then
+        import dir
+      else mkModules "${dir}/${module}";
+    })
+  ) (builtins.attrNames (builtins.readDir dir))
+);
 
 in
-mkModules ./.
+{ options.modules.user = mkModules ./.; }
