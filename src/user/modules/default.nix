2a16ae0a9d64e3e71765900081c8ba6a51aeef99
Bryan Ramos <bryan@ramos.codes>
2024-05-15 20:35:52 -0400

    mkModules impl

--- src/user/modules/default.nix
+++ src/user/modules/default.nix
@@ -1,9 +1,42 @@
-{
-  imports = [
-    ./bash
-    ./git
-    ./gui
-    ./security
-    ./utils
-  ];
-}
+{ lib, ... }:
+
+with lib;
+let
+  # Function to list the contents of a directory
+  listDir = dir: builtins.filter (path: builtins.pathExists (dir + "/" + path)) (builtins.readDir dir);
+
+  # Function to generate Nix attribute sets for directories
+  traverse = dir: pathList: {
+    inherit dir;
+    contents = builtins.foldl' (acc: path:
+    let
+      fullPath = dir + "/" + path;
+      newPathList = pathList ++ [ path ];
+      optionPath = builtins.concatStringsSep "." newPathList;
+      cfgPath = builtins.concatStringsSep "." (["config" "modules" "user"] ++ newPathList);
+    in
+      if builtins.pathExists fullPath && builtins.isDirectory fullPath then
+        if path == "modules" then
+          acc // { "${path}" = traverse fullPath newPathList; }
+        else
+          acc
+      else if path == "default.nix" then
+        let
+          userDefinedAttrs = builtins.evalFile fullPath;
+        in
+          acc // { "${builtins.head pathList}" = {
+            enable = mkEnableOption "user.${optionPath}";
+            config = mkIf (eval "${cfgPath}.enable") userDefinedAttrs;
+          }; }
+      else
+        acc
+    ) {} (listDir dir);
+  };
+
+  # Function to generate the entire options.modules.user attribute set
+  mkModules = dir: {
+    options.modules.user = traverse dir [];
+  };
+in
+# Example usage
+mkModules ./.
