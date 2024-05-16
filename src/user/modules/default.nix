2f3dfafd3f07711424507f131b8a47aad11b385e
Bryan Ramos <bryan.ramos@concurrent-rt.com>
2024-05-16 15:38:37 -0400

    mkModules implemented

--- src/user/modules/default.nix
+++ src/user/modules/default.nix
@@ -1,18 +1,33 @@
-{ lib, ... }:
+{ lib, config, ... }:
 
-with lib;
 let
 
-mkModules = dir: (
-  listToAttrs (
-    map (module: {
-      name = module;
-      value = if module == "default.nix" then
-        import dir
-      else mkModules "${dir}/${module}";
-    })
+moduleType = "user";
+
+mkModules = dir: pathAcc: (
+  builtins.listToAttrs (
+    builtins.filter (x: x != null) (
+      map (node:
+      let
+        path = dir + (if dir == ./. then null else "/") + node;
+        acc = if pathAcc == "" then node else pathAcc + "." + node;
+      in
+        if lib.isDirectory path && node != "config" then
+          if node != "modules" then {
+            name = lib.baseNameOf path;
+            value = {
+              options.modules.user.${acc}.enable = lib.mkEnableOption "${moduleType}.${acc}";
+              config = lib.mkIf config.modules.user.${acc} (import path);
+            };
+          } + (mkModules path acc)
+          else
+            mkModules path pathAcc
+        else
+          null
+      )
+    )
   ) (builtins.attrNames (builtins.readDir dir))
 );
 
 in
-{ options.modules.user = mkModules ./.; }
+mkModules ./.
