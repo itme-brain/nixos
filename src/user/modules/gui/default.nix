7fb003982fbc326eb59e5178a2bc78c0d6e4211d
Bryan Ramos <bryan@ramos.codes>
2024-06-14 10:44:19 -0400

    added gui/modules wm check

--- src/user/modules/gui/default.nix
+++ src/user/modules/gui/default.nix
@@ -1,6 +1,17 @@
+{ config, ... }:
+
+let
+  gui = config.modules.user.gui.wm;
+
+  wm = {
+    enable = builtins.any (mod: mod.enable or false) (builtins.attrValues gui);
+  };
+
+in
 {
   imports = [
     ./wm
+  ] ++ (if wm.enable then [
     ./modules
-  ];
+  ] else []);
 }
