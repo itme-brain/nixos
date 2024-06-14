648f7a8937b73553481921d7f22ab8314055b918
Bryan Ramos <bryan@ramos.codes>
2024-06-14 11:22:51 -0400

    attempt to fix recursion problem

--- src/user/modules/gui/default.nix
+++ src/user/modules/gui/default.nix
@@ -1,17 +1,6 @@
-{ config, ... }:
-
-let
-  gui = config.modules.user.gui.wm;
-
-  wm = {
-    enable = builtins.any (mod: mod.enable or false) (builtins.attrValues gui);
-  };
-
-in
 {
   imports = [
     ./wm
-  ] ++ (if wm.enable then [
     ./modules
-  ] else []);
+  ];
 }
--- src/user/modules/gui/modules/default.nix
+++ src/user/modules/gui/modules/default.nix
@@ -1,10 +1,18 @@
+{ config, lib, ... }:
+
+let
+  wm = {
+    enable = builtins.any (mod: mod.enable or false) (builtins.attrValues config.modules.user.gui.wm);
+  };
+
+in
 {
-  imports = [
+  imports = if wm.enable then [
     ./alacritty
     ./browsers
     ./corn
     ./fun
     ./utils
     ./writing
-  ];
+  ] else [];
 }
