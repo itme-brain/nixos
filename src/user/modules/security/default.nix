00dc854cf1507a8614ea267f0b2b77b94445eca2
Bryan Ramos <bryan@ramos.codes>
2024-06-15 17:15:44 -0400

    Added pass extensions

--- src/user/modules/security/default.nix
+++ src/user/modules/security/default.nix
@@ -3,9 +3,15 @@
 with lib;
 let
   cfg = config.modules.user.security;
+  pass = pkgs.pass.withExtensions (exts: with exts; [
+    pass-audit
+    pass-otp
+    pass-update
+    pass-tomb
+  ]);
 
 in
-{ options.modules.user.security = { enable = mkEnableOption "user.security"; };
+{ options.modules.user.security = { enable = mkEnableOption "Enable security module"; };
   imports = [ ./modules ];
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
