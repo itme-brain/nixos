84ffea034d5e2c0ff6f4d60d2560093945e00067
Bryan Ramos <bryan@ramos.codes>
2024-10-07 08:54:34 -0400

    cmp keymaps

--- src/user/modules/utils/modules/neovim/config/lazyvim/lua/plugins/lsp.lua
+++ src/user/modules/utils/modules/neovim/config/lazyvim/lua/plugins/lsp.lua
@@ -130,8 +130,8 @@ return {
         },
 
         mapping = cmp.mapping.preset.insert({
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-e>"] = cmp.mapping.abort(),
