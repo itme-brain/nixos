6d9d2fe824308578c6428d622e29794f9ef70a2c
Bryan Ramos <bryan@ramos.codes>
2024-11-05 06:00:59 -0500

    removed let variable for readability

--- src/user/configs/default.nix
+++ src/user/configs/default.nix
@@ -4,33 +4,31 @@ with lib;
 let
 modules = config.modules.user;
 
-userConfigs = rec {
-  name = "bryan";
-  email = "bryan@ramos.codes";
-  shell = pkgs.bash;
-
-  groups = [ "wheel" "networkmanager" "home-manager" "input" ];
-
-  gitConfig = optionalAttrs modules.git.enable {
-    userName = "Bryan Ramos";
-    userEmail = email;
-    signing = optionalAttrs modules.security.gpg.enable {
-      key = "F1F3466458452B2DF351F1E864D12BA95ACE1F2D";
-      signByDefault = true;
-    };
-  };
-
-  keys = import ./keys;
-  bookmarks = import ./bookmarks;
-};
-
 in
 {
   options = {
     user = mkOption {
       description = "User Configurations";
       type = types.attrs;
-      default = userConfigs;
+      default = rec {
+        name = "bryan";
+        email = "bryan@ramos.codes";
+        shell = pkgs.bash;
+        keys = import ./keys;
+
+        groups = [ "wheel" "networkmanager" "home-manager" "input" ];
+
+        gitConfig = optionalAttrs modules.git.enable {
+          userName = "Bryan Ramos";
+          userEmail = email;
+          signing = optionalAttrs modules.security.gpg.enable {
+            key = "F1F3466458452B2DF351F1E864D12BA95ACE1F2D";
+            signByDefault = true;
+          };
+        };
+
+        bookmarks = import ./bookmarks;
+      };
     };
   };
 }
