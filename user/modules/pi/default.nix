12c8f712e30f6de713225e9cb4c58bb9d2f91656
Bryan Ramos <bryan@ramos.codes>
2026-04-30 21:43:28 -0400

    updated

--- user/modules/pi/default.nix
+++ user/modules/pi/default.nix
@@ -4,6 +4,8 @@ with lib;
 let
   cfg = config.modules.user.pi;
   npmGlobal = "${config.home.homeDirectory}/.npm-global";
+  piVersion = "0.70.5";
+
 in
 { options.modules.user.pi = { enable = mkEnableOption "user.pi"; };
   config = mkIf cfg.enable {
@@ -23,6 +25,14 @@ in
 
     programs.bash.initExtra = ''
       export LLAMACPP_API_KEY=$(cat /run/secrets/LLAMA_API_KEY)
+    '';
+
+    home.activation.installPiCodingAgent = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      PATH="${pkgs.nodejs_20}/bin:$PATH"
+      run mkdir -p ${npmGlobal}
+      if ! run ${pkgs.nodejs_20}/bin/npm install -g --prefix ${npmGlobal} @mariozechner/pi-coding-agent@${piVersion}; then
+        warnEcho "pi-coding-agent install failed (offline or registry error)"
+      fi
     '';
   };
 }
